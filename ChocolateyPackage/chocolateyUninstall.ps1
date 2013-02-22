8f126b62f04362abe18bf4d16568ede461f324ff
Gary Ewan Park <gep13@gep13.co.uk>
2013-02-22 07:07:23 +0000

    Issue #3 - Added uninstall script
    Added the necessary PowerShell code to look up the LocalPackage source
    of the installed application, and then uninstall it.

---
+++ ChocolateyPackage/chocolateyUninstall.ps1
@@ -0,0 +1,20 @@
+$package = "ChocolateyGUI"
+
+try {
+  	$packageGuid = Get-ChildItem HKLM:\SOFTWARE\Classes\Installer\Products |
+    	Get-ItemProperty -Name 'ProductName' |
+    	? { $_.ProductName -eq $package } |
+    	Select -ExpandProperty PSChildName -First 1
+	
+	$properties = Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Products\$packageGuid\InstallProperties
+	
+	$pkg = $properties.LocalPackage
+	
+	msiexec.exe /x $pkg /qr /norestart
+	
+	Write-ChocolateySuccess $package
+}
+catch {
+	Write-ChocolateyFailure $package "$($_.Exception.Message)"
+	throw
+}

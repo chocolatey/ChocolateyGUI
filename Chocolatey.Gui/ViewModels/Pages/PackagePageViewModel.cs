7106c418d0e5265014f031a4ef48614a5ba8345c
Richard Simpson <richardsimpson@outlook.com>
2014-02-08 18:57:58 -0600

    #94 Started work

---
+++ Chocolatey.Gui/ViewModels/Pages/PackagePageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Chocolatey.Gui.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chocolatey.Gui.ViewModels.Pages
+{
+    public class PackagePageViewModel : ObservableBase, IPackagePageViewModel
+    {
+    }
+}

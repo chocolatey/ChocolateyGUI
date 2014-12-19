7973e5535f5025fc7bee65a4a9293bedf50945a8
Alexander Ulitin <alexander.a.ulitin@gmail.com>
2014-12-19 23:35:27 +0300

    Checkbox replaced with tick/cross chars

---
+++ Source/ChocolateyGui/Utilities/Converters/BooleanToTickString.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Chocolatey" file="BooleanToTickString.cs">
+//   Copyright 2014 - Present Rob Reynolds, the maintainers of Chocolatey, and RealDimensions Software, LLC
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ChocolateyGui.Utilities.Converters
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Data;
+
+    public class BooleanToTickString : DependencyObject, IValueConverter
+    {
+        private const string Tick = "✓";
+
+        private const string Cross = "✗";
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (bool)value ? Tick : Cross;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

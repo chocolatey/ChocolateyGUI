812bd633add746ab6c99ca15c3c6c8640f095dba
Richard Simpson <richard.simpson@finalphasesystems.com>
2015-10-02 21:37:19 -0500

    (GH-283) Use NuGet to retrieve Resharper CLI tools

--- BuildScripts/default.ps1
+++ BuildScripts/default.ps1
@@ -293,7 +293,7 @@ Task -Name __InstallReSharperCommandLineTools -Depends __InstallChocolatey -Desc
 
 		if (-not (Test-Path $inspectCodeExe)) {
 			exec {
-				Invoke-Expression "$script:chocolateyCommand install resharper-clt -y";
+				Invoke-Expression "$script:chocolateyCommand install JetBrains.ReSharper.CommandLineTools -source https://www.nuget.org/api/v2/ -y";
 			}
 		} else {
 			Write-Output "resharper-clt already installed";
@@ -312,7 +312,7 @@ Task -Name __UpdateReSharperCommandLineTools -Description $private -Action {
 		Write-Output "Running Upgrade Command Line Tools..."
 
 		exec {
-			Invoke-Expression "$script:chocolateyCommand upgrade resharper-clt";
+			Invoke-Expression "$script:chocolateyCommand upgrade JetBrains.ReSharper.CommandLineTools -source https://www.nuget.org/api/v2/";
 		}
 
 		Write-Output ("************ Upgrade Command Line Tools Successful ************")

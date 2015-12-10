017cc726a71b2db4ba8214f532199e0f38489c69
Gary Ewan Park <gep13@gep13.co.uk>
2015-12-10 20:02:30 +0000

    (build) Mattias suggestion, number 1

--- BuildScripts/default.ps1
+++ BuildScripts/default.ps1
@@ -337,7 +337,7 @@ Task -Name __InstallPSBuild -Description $private -Action {
 				Write-Output "PSBuild is already installed";
 			} else {
       	Write-Output "PSBuild is not already installed";
-        (new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ligershark/psbuild/master/src/GetPSBuild.ps1') | Invoke-Expression;
+        (new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ligershark/psbuild/203670868f76dbec313c171bdd6100ac91bd2963/src/GetPSBuild.ps1') | Invoke-Expression;
 			}
 		}
 

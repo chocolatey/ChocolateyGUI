2625fe0076f8fa86a4cb590731de7c85d6fc3405
Robin Schlegel <rob100@web.de>
2015-12-23 13:26:12 +0100

    (GH-288) Improved GUI behaviour after "update all"
    After "update all" there will be a "Refresh packages" to show the
    updated packages correctly (e.g. updated version information) and "Show
    Only Packages with Update" will be unchecked

--- Source/ChocolateyGui/ViewModels/Controls/LocalSourceControlViewModel.cs
+++ Source/ChocolateyGui/ViewModels/Controls/LocalSourceControlViewModel.cs
@@ -188,6 +188,8 @@ namespace ChocolateyGui.ViewModels.Controls
                 }
 
                 await this._progressService.StopLoading();
+                this.ShowOnlyPackagesWithUpdate = false;
+                this.RefreshPackages();
             }
             catch (Exception ex)
             {

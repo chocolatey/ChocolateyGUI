9109f92b4beede59cf5a8a995123c6ed5224c64c
Richard Simpson <richardsimpson@outlook.com>
2016-12-20 10:00:59 -0600

    Fix sources visual desync.

--- Source/ChocolateyGui/Views/SettingsView.xaml.cs
+++ Source/ChocolateyGui/Views/SettingsView.xaml.cs
@@ -4,16 +4,25 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
+using Caliburn.Micro;
+using ChocolateyGui.Models.Messages;
+
 namespace ChocolateyGui.Views
 {
     /// <summary>
     /// Interaction logic for SettingsView.xaml
     /// </summary>
-    public partial class SettingsView
+    public partial class SettingsView : IHandle<SourcesUpdatedMessage>
     {
-        public SettingsView()
+        public SettingsView(IEventAggregator eventAggregator)
         {
+            eventAggregator.Subscribe(this);
             InitializeComponent();
+        }
+
+        public void Handle(SourcesUpdatedMessage message)
+        {
+            SourcesGrid.Items.Refresh();
         }
     }
 }

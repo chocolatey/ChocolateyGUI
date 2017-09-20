a309033790761af861d2dca42d593a8001076089
Gary Ewan Park <gep13@gep13.co.uk>
2017-09-20 09:20:15 +0100

    (maint) Spaces to Tabs

--- nuspec/chocolatey/chocolateyInstall.ps1
+++ nuspec/chocolatey/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 $scriptPath =  $(Split-Path $MyInvocation.MyCommand.Path)
 
 $packageArgs = @{
-  packageName = 'ChocolateyGUI'
-  softwareName   = 'ChocolateyGUI'
-  fileType = 'msi'
-  silentArgs = '/quiet'
-  file = Join-Path $scriptPath 'ChocolateyGUI.msi'
+	packageName = 'ChocolateyGUI'
+	softwareName   = 'ChocolateyGUI'
+	fileType = 'msi'
+	silentArgs = '/quiet'
+	file = Join-Path $scriptPath 'ChocolateyGUI.msi'
 }
 
 Install-ChocolateyInstallPackage @packageArgs

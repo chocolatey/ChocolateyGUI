670d70072fc64e297ed067701110f012d96e596b
Jan Karger <punkerat76@gmail.com>
2017-11-01 14:03:03 +0100

    (GH-517) Fix sorting for Title and Authors

---
+++ Source/ChocolateyGui/Utilities/IDataGridColumnComparer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.ComponentModel;
+
+namespace ChocolateyGui.Utilities
+{
+    public interface IDataGridColumnComparer : IComparer
+    {
+        ListSortDirection SortDirection { get; set; }
+    }
+}

16bf58335f18ff2e7f992dbf48c4d4c668582326
DamianMaslanka5 <damian.maslanka5@gmail.com>
2018-10-06 22:05:56 +0200

    Work on copy of selected source.

--- Source/ChocolateyGui/Models/ChocolateySource.cs
+++ Source/ChocolateyGui/Models/ChocolateySource.cs
@@ -10,6 +10,15 @@ namespace ChocolateyGui.Models
 {
     public class ChocolateySource : IEquatable<ChocolateySource>
     {
+        public ChocolateySource(ChocolateySource source)
+        {
+            Update(source);
+        }
+
+        public ChocolateySource()
+        {
+        }
+
         public string Id { get; set; }
 
         public string Value { get; set; }
@@ -94,6 +103,26 @@ namespace ChocolateyGui.Models
                 hashCode = (hashCode * 397) ^ VisibleToAdminsOnly.GetHashCode();
                 return hashCode;
             }
+        }
+
+        public void Update(ChocolateySource source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            Id = source.Id;
+            Value = source.Value;
+            Disabled = source.Disabled;
+            UserName = source.UserName;
+            Password = source.Password;
+            Priority = source.Priority;
+            Certificate = source.Certificate;
+            CertificatePassword = source.CertificatePassword;
+            BypassProxy = source.BypassProxy;
+            AllowSelfService = source.AllowSelfService;
+            VisibleToAdminsOnly = source.VisibleToAdminsOnly;
         }
     }
 }

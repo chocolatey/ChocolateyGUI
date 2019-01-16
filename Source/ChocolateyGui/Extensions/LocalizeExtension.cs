c177b36eb66d9270df79bfd8a7aee03a6b52cb76
AdmiringWorm <kim.nordmo@gmail.com>
2019-01-16 12:21:18 +0100

    (GH-533) Added ability to bind to dynamic language strings

---
+++ Source/ChocolateyGui/Extensions/LocalizeExtension.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Chocolatey" file="LocalizeExtension.cs">
+//   Copyright 2014 - Present Rob Reynolds, the maintainers of Chocolatey, and RealDimensions Software, LLC
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Windows.Data;
+using ChocolateyGui.Utilities;
+
+namespace ChocolateyGui.Extensions
+{
+    public sealed class LocalizeExtension : Binding
+    {
+        public LocalizeExtension(string name)
+            : base("[" + name + "]")
+        {
+            Mode = BindingMode.OneWay;
+            Source = TranslationSource.Instance;
+        }
+    }
+}

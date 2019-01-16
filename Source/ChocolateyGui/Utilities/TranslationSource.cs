d044fd05ef2c52a6bb0fd4fb6427b3498871d564
AdmiringWorm <kim.nordmo@gmail.com>
2019-01-16 12:18:19 +0100

    (GH-533) Added ability to notify when language is changed

---
+++ Source/ChocolateyGui/Utilities/TranslationSource.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Chocolatey" file="TranslationSource.cs">
+//   Copyright 2014 - Present Rob Reynolds, the maintainers of Chocolatey, and RealDimensions Software, LLC
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Globalization;
+using System.Resources;
+using ChocolateyGui.Base;
+using ChocolateyGui.Properties;
+
+namespace ChocolateyGui.Utilities
+{
+    public sealed class TranslationSource : ObservableBase
+    {
+        private readonly ResourceManager resourceManager = Resources.ResourceManager;
+        private CultureInfo currentCulture = null;
+
+        public static TranslationSource Instance { get; } = new TranslationSource();
+
+        public CultureInfo CurrentCulture
+        {
+            get
+            {
+                return currentCulture;
+            }
+
+            set
+            {
+                if (currentCulture != value)
+                {
+                    currentCulture = value;
+                    NotifyPropertyChanged(string.Empty); // We call with an empty string on purpose to force an update for all resource strings.
+                }
+            }
+        }
+
+        public string this[string key] => resourceManager.GetString(key, CurrentCulture);
+    }
+}

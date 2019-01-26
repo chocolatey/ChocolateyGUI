c66ed83b70b0a038a70a5884b56dc31f566ed0fc
Manfred Wallner <schusterfredl@mwallner.net>
2019-01-26 13:53:18 +0100

    (GH-588) add option to show aggregated source view

---
+++ Source/ChocolateyGui/Models/ChocolateyAggregatedSources.cs
@@ -0,0 +1,16 @@
+﻿using ChocolateyGui.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChocolateyGui.Models
+{
+    public class ChocolateyAggregatedSources : ChocolateySource
+    {
+        public ChocolateyAggregatedSources() {
+            Id = Resources.SourcesView_AggregatedSourcesId;
+        }
+    }
+}

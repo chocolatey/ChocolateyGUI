32f620130e505d81624b0f7f4d689817a1375928
Gary Ewan Park <gep13@gep13.co.uk>
2019-05-03 07:46:05 +0100

    (GH-525) Add new classes for upcoming work
    Most of these are borrowed from the Command implementation within
    Chocolatey itself, so that there is similarities in how they are
    implemented.  However, the decision was taken to not re-use the base
    classes from Chocolatey directly due to the potential for things to
    change in the future.

---
+++ Source/ChocolateyGui/CliCommands/ConfigCommandType.cs
@@ -0,0 +1,11 @@
+﻿namespace ChocolateyGui.CliCommands
+{
+    public enum ConfigCommandType
+    {
+        unknown,
+        list,
+        get,
+        set,
+        unset,
+    }
+}
---
+++ Source/ChocolateyGui/CliCommands/FeatureCommandType.cs
@@ -0,0 +1,10 @@
+﻿namespace ChocolateyGui.CliCommands
+{
+    public enum FeatureCommandType
+    {
+        unknown,
+        list,
+        enable,
+        disable,
+    }
+}

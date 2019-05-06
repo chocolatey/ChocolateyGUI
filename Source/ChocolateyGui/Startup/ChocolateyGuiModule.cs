57af012a2d110ac65463b6d74df91721477bf938
Gary Ewan Park <gep13@gep13.co.uk>
2019-05-06 10:57:42 +0100

    (maint) Fix inspectcode errors

--- Source/ChocolateyGui/Startup/ChocolateyGuiModule.cs
+++ Source/ChocolateyGui/Startup/ChocolateyGuiModule.cs
@@ -5,7 +5,6 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using Autofac;

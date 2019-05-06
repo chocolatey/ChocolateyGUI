1245590d4553b3f9d56db8ed3bb4af5278ac6c46
Gary Ewan Park <gep13@gep13.co.uk>
2019-05-06 15:37:20 +0100

    (GH-592) Add Package Parameters to install
    So that during installation, end user can make a decision about what
    features are enabled/disabled in Chocolatey GUI, as well as set any
    configuration of Chocolatey GUI.
    
    Chocolatey dependency was changed to ensure Chocolatey version of
    Get-PackageParameters is used.

--- nuspec/chocolatey/chocolateyInstall.ps1
+++ nuspec/chocolatey/chocolateyInstall.ps1
@@ -20,3 +20,71 @@ $installDirectory = Get-AppInstallLocation $packageArgs.softwareName
 if ($installDirectory) {
   Install-BinFile -Name "chocolateygui" -Path "$installDirectory\ChocolateyGui.exe" -UseStart
 }
+
+$pp = Get-PackageParameters
+
+# Features
+if($pp.ContainsKey("ShowConsoleOutput")) {
+  if($pp.ShowConsoleOutput -eq $true) {
+    Write-Output "Enabling ShowConsoleOutput..."
+    Start-ChocolateyProcessAsAdmin -Statements "feature enable --name=ShowConsoleOutput" -ExeToRun "chocolateygui"
+  } else {
+    Write-Output "Disabling ShowConsoleOutput..."
+    Start-ChocolateyProcessAsAdmin -Statements "feature disable --name=ShowConsoleOutput" -ExeToRun "chocolateygui"
+
+  }
+}
+
+if($pp.ContainsKey("DefaultToTileViewForLocalSource")) {
+  if($pp.DefaultToTileViewForLocalSource -eq $true) {
+    Write-Output "Enabling DefaultToTileViewForLocalSource..."
+    Start-ChocolateyProcessAsAdmin -Statements "feature enable --name=DefaultToTileViewForLocalSource" -ExeToRun "chocolateygui"
+  } else {
+    Write-Output "Disabling DefaultToTileViewForLocalSource..."
+    Start-ChocolateyProcessAsAdmin -Statements "feature disable --name=DefaultToTileViewForLocalSource" -ExeToRun "chocolateygui"
+  }
+}
+
+if($pp.ContainsKey("DefaultToTileViewForRemoteSource")) {
+  if($pp.DefaultToTileViewForRemoteSource -eq $true) {
+    Write-Output "Enabling DefaultToTileViewForRemoteSource..."
+    Start-ChocolateyProcessAsAdmin -Statements "feature enable --name=DefaultToTileViewForRemoteSource" -ExeToRun "chocolateygui"
+  } else {
+    Write-Output "Disabling DefaultToTileViewForRemoteSource..."
+    Start-ChocolateyProcessAsAdmin -Statements "feature disable --name=DefaultToTileViewForRemoteSource" -ExeToRun "chocolateygui"
+  }
+}
+
+if($pp.ContainsKey("UseDelayedSearch")) {
+  if($pp.UseDelayedSearch -eq $true) {
+    Write-Output "Enabling UseDelayedSearch..."
+    Start-ChocolateyProcessAsAdmin -Statements "feature enable --name=UseDelayedSearch" -ExeToRun "chocolateygui"
+  } else {
+    Write-Output "Disabling UseDelayedSearch..."
+    Start-ChocolateyProcessAsAdmin -Statements "feature disable --name=UseDelayedSearch" -ExeToRun "chocolateygui"
+  }
+}
+
+if($pp.ContainsKey("ExcludeInstalledPackages")) {
+  if($pp.ExcludeInstalledPackages -eq $true) {
+    Write-Output "Enabling ExcludeInstalledPackages..."
+    Start-ChocolateyProcessAsAdmin -Statements "feature enable --name=ExcludeInstalledPackages" -ExeToRun "chocolateygui"
+  } else {
+    Write-Output "Disabling ExcludeInstalledPackages..."
+    Start-ChocolateyProcessAsAdmin -Statements "feature disable --name=ExcludeInstalledPackages" -ExeToRun "chocolateygui"
+  }
+}
+
+if($pp.ContainsKey("ShowAggregatedSourceView")) {
+  if($pp.ShowAggregatedSourceView -eq $true) {
+    Write-Output "Enabling ShowAggregatedSourceView..."
+    Start-ChocolateyProcessAsAdmin -Statements "feature enable --name=ShowAggregatedSourceView" -ExeToRun "chocolateygui"
+  } else {
+    Write-Output "Disabling ShowAggregatedSourceView..."
+    Start-ChocolateyProcessAsAdmin -Statements "feature disable --name=ShowAggregatedSourceView" -ExeToRun "chocolateygui"
+  }
+}
+
+if($pp.ContainsKey("OutputPackagesCacheDurationInMinutes")) {
+  Start-ChocolateyProcessAsAdmin -Statements "config set --name=OutputPackagesCacheDurationInMinutes --value=$($pp.OutputPackagesCacheDurationInMinutes)" -ExeToRun "chocolateygui"
+}

95380260ba3e30423ee2afdfd46625558e0650d9
Gary Ewan Park <gep13@gep13.co.uk>
2020-04-09 13:39:38 +0100

    (GH-757) Introduce interface for allowed commands
    This will be used to return "what" commands are allowed.  In the
    current OSS version of Chocolatey GUI, these are all true, however,
    this provides the ability to control this in the future, if required.

---
+++ Source/ChocolateyGui.Common/Services/IAllowedCommandsService.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Chocolatey" file="IAllowedCommandsService.cs">
+//   Copyright 2017 - Present Chocolatey Software, LLC
+//   Copyright 2014 - 2017 Rob Reynolds, the maintainers of Chocolatey, and RealDimensions Software, LLC
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ChocolateyGui.Common.Services
+{
+    public interface IAllowedCommandsService
+    {
+        bool IsPinCommandAllowed { get; }
+
+        bool IsInstallCommandAllowed { get; }
+
+        bool IsUninstallCommandAllowed { get; }
+
+        bool IsUpgradeCommandAllowed { get; }
+    }
+}

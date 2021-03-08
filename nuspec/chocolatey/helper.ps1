862e1d2c4397d1aacdab003549f9724d3603c30a
Gary Ewan Park <gep13@gep13.co.uk>
2021-03-08 14:51:11 +0000

    (maint) Remove unnecessary whitespace

--- nuspec/chocolatey/helper.ps1
+++ nuspec/chocolatey/helper.ps1
@@ -7,7 +7,7 @@ function Set-FeatureState {
         [Parameter(Mandatory=$true)]
         [bool] $EnableFeature
     )
-  
+
     if ($EnableFeature) {
         Write-Output "Enabling $FeatureName..."
         Start-ChocolateyProcessAsAdmin -Statements "feature enable --name=$FeatureName" -ExeToRun "chocolateyguicli"
@@ -21,37 +21,37 @@ function Set-UserSettings {
     [CmdletBinding()]
     param(
         [hashtable]$pp = ( Get-PackageParameters )
-    
+
     )
     # Features
     if($pp.ContainsKey("ShowConsoleOutput")) {
         Set-FeatureState "ShowConsoleOutput" ($pp.ShowConsoleOutput -eq $true)
     }
-    
+
     if($pp.ContainsKey("DefaultToTileViewForLocalSource")) {
         Set-FeatureState "DefaultToTileViewForLocalSource" ($pp.DefaultToTileViewForLocalSource -eq $true)
     }
-    
+
     if($pp.ContainsKey("DefaultToTileViewForRemoteSource")) {
         Set-FeatureState "DefaultToTileViewForRemoteSource" ($pp.DefaultToTileViewForRemoteSource -eq $true)
     }
-    
+
     if($pp.ContainsKey("UseDelayedSearch")) {
         Set-FeatureState "UseDelayedSearch" ($pp.UseDelayedSearch -eq $true)
     }
-    
+
     if($pp.ContainsKey("PreventPreload")) {
         Set-FeatureState "PreventPreload" ($pp.PreventPreload -eq $true)
     }
-    
+
     if($pp.ContainsKey("PreventAutomatedOutdatedPackagesCheck")) {
         Set-FeatureState "PreventAutomatedOutdatedPackagesCheck" ($pp.PreventAutomatedOutdatedPackagesCheck -eq $true)
     }
-    
+
     if($pp.ContainsKey("ExcludeInstalledPackages")) {
         Set-FeatureState "ExcludeInstalledPackages" ($pp.ExcludeInstalledPackages -eq $true)
     }
-    
+
     if($pp.ContainsKey("ShowAggregatedSourceView")) {
         Set-FeatureState "ShowAggregatedSourceView" ($pp.ShowAggregatedSourceView -eq $true)
     }
@@ -72,7 +72,7 @@ function Set-UserSettings {
         Set-FeatureState "HidePackageDownloadCount" ($pp.HidePackageDownloadCount -eq $true)
     }
 
-    # config    
+    # config
     if($pp.ContainsKey("OutdatedPackagesCacheDurationInMinutes")) {
         Start-ChocolateyProcessAsAdmin -Statements "config set --name=OutdatedPackagesCacheDurationInMinutes --value=$($pp.OutdatedPackagesCacheDurationInMinutes)" -ExeToRun "chocolateyguicli"
     }

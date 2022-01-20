a2112121f6d4851a085de4c5854b404630600142
AdmiringWorm <kim@chocolatey.io>
2022-01-20 12:03:19 +0100

    (#533) Update internalization module to use existing resources
    This commit updates the class module for Internationalization to
    look at installation location of Chocolatey GUI and only collect the
    languages that have been installed by the program.

--- Source/ChocolateyGui.Common.Windows/Startup/Internationalization.cs
+++ Source/ChocolateyGui.Common.Windows/Startup/Internationalization.cs
@@ -5,27 +5,113 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
+using System.Linq;
 using System.Threading;
-using ChocolateyGui.Common.Windows.Utilities;
+using ChocolateyGui.Common.Utilities;
 
 namespace ChocolateyGui.Common.Windows.Startup
 {
     public static class Internationalization
     {
+        private static readonly HashSet<CultureInfo> _cachedCultures = new HashSet<CultureInfo>();
+        private static readonly Lazy<CultureInfo> _fallbackCulture = new Lazy<CultureInfo>(() => new CultureInfo("en"));
+
+        public static event EventHandler<CultureInfo> LanguageChanged;
+
+        public static IEnumerable<CultureInfo> GetAllSupportedCultures()
+        {
+            if (_cachedCultures.Count > 0)
+            {
+                return _cachedCultures;
+            }
+
+            _cachedCultures.Add(GetFallbackCulture());
+
+            var installLocation = Bootstrapper.ApplicationFilesPath;
+
+            foreach (var directory in Directory.EnumerateDirectories(installLocation))
+            {
+                var resourceAssemblyPath = Path.Combine(directory, "ChocolateyGui.Common.resources.dll");
+                if (!File.Exists(resourceAssemblyPath))
+                {
+                    continue;
+                }
+
+                try
+                {
+                    var directoryName = Path.GetFileName(directory);
+
+                    var culture = new CultureInfo(directoryName);
+
+                    if (!_cachedCultures.Contains(culture))
+                    {
+                        _cachedCultures.Add(culture);
+                    }
+                }
+                catch (Exception)
+                {
+                    // Ignored on purpose.
+                }
+            }
+
+            return _cachedCultures;
+        }
+
+        public static CultureInfo GetFallbackCulture()
+        {
+            return _fallbackCulture.Value;
+        }
+
+        public static CultureInfo GetSupportedCultureInfo(string languageCode)
+        {
+            if (string.IsNullOrEmpty(languageCode))
+            {
+                return GetFallbackCulture();
+            }
+
+            var cultureInfo = new CultureInfo(languageCode);
+
+            var foundCulture = GetAllSupportedCultures().FirstOrDefault(c =>
+                string.Equals(c.Name, cultureInfo.Name, StringComparison.OrdinalIgnoreCase));
+
+            if (foundCulture != null)
+            {
+                return foundCulture;
+            }
+
+            if (!string.IsNullOrEmpty(cultureInfo.Parent.Name))
+            {
+                return GetSupportedCultureInfo(cultureInfo.Parent.Name);
+            }
+
+            return GetFallbackCulture();
+        }
+
         public static void Initialize()
         {
-            TranslationSource.Instance.CurrentCulture = CultureInfo.CurrentCulture;
+            UpdateLanguage(CultureInfo.CurrentCulture.Name);
         }
 
         public static void UpdateLanguage(string languageCode)
         {
-            var culture = CultureInfo.GetCultureInfo(languageCode);
-            TranslationSource.Instance.CurrentCulture = culture;
-            CultureInfo.DefaultThreadCurrentCulture = culture;
-            CultureInfo.DefaultThreadCurrentUICulture = culture;
-            Thread.CurrentThread.CurrentUICulture = culture;
-            Thread.CurrentThread.CurrentCulture = culture;
+            var existingLanguage = TranslationSource.Instance.CurrentCulture ?? CultureInfo.CurrentCulture;
+
+            var culture = GetSupportedCultureInfo(languageCode);
+
+            if (culture != existingLanguage)
+            {
+                TranslationSource.Instance.CurrentCulture = culture;
+                CultureInfo.DefaultThreadCurrentCulture = culture;
+                CultureInfo.DefaultThreadCurrentUICulture = culture;
+                Thread.CurrentThread.CurrentUICulture = culture;
+                Thread.CurrentThread.CurrentCulture = culture;
+
+                LanguageChanged?.Invoke(typeof(Internationalization), culture);
+            }
         }
     }
 }

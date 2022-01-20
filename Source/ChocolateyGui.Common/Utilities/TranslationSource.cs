40dfa400c1a7a271ab49ed2790b0c127bd9a679e
AdmiringWorm <kim@chocolatey.io>
2022-01-20 11:46:51 +0100

    (#533) Update TranslationSource to accept parameters
    Quite a few of the strings that are being used for
    translations accepts parameters as such it would
    be helpful if the translation source accepts an
    undefined number of arguments instead of having
    to call a manually a different formatting method.
    
    This commit updates that translation source to accept
    source parameters.

---
+++ Source/ChocolateyGui.Common/Utilities/TranslationSource.cs
@@ -0,0 +1,82 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Chocolatey" file="TranslationSource.cs">
+//   Copyright 2014 - Present Rob Reynolds, the maintainers of Chocolatey, and RealDimensions Software, LLC
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using chocolatey;
+using ChocolateyGui.Common.Annotations;
+using ChocolateyGui.Common.Properties;
+
+namespace ChocolateyGui.Common.Utilities
+{
+    public sealed class TranslationSource : INotifyPropertyChanged
+    {
+        private static readonly Lazy<TranslationSource> _instance =
+            new Lazy<TranslationSource>(() => new TranslationSource());
+        private readonly ResourceManager _resourceManager = Resources.ResourceManager;
+        private CultureInfo _currentCulture;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public static TranslationSource Instance { get; } = _instance.Value;
+
+        public CultureInfo CurrentCulture
+        {
+            get
+            {
+                return _currentCulture;
+            }
+
+            set
+            {
+                if (!Equals(_currentCulture, value))
+                {
+                    _currentCulture = value;
+                    NotifyPropertyChanged(string.Empty); // We call with an empty string on purpose to force an update for all resource strings.
+                }
+            }
+        }
+
+        public string this[string key]
+        {
+            get
+            {
+                var value = _resourceManager.GetString(key, CurrentCulture);
+#if DEBUG
+                if (string.IsNullOrEmpty(value) && !string.IsNullOrEmpty(key))
+                {
+                    return "[" + key + "]";
+                }
+#endif
+                return value;
+            }
+        }
+
+        public string this[string key, params object[] parameters]
+        {
+            get
+            {
+                var value = this[key];
+
+                if (parameters != null && parameters.Length > 0)
+                {
+                    return value.format_with(parameters);
+                }
+
+                return value;
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

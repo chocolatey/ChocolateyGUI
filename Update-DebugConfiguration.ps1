5c1aa32f42886deacc67d00fa80dfc27d6ac23eb
Gary Ewan Park <gep13@gep13.co.uk>
2022-09-13 08:12:07 +0100

    (#902) Correctly output the version number
    Without this change, the output contains both the package id and
    version number, when the intention is simply to output the version
    number.

--- Update-DebugConfiguration.ps1
+++ Update-DebugConfiguration.ps1
@@ -3,7 +3,7 @@ $NugetPackage = $ChocolateyPackages | Where-Object Package -EQ nuget.commandline
 $ChocolateyVersion = $ChocolateyPackages | Where-Object Package -EQ 'chocolatey'
 
 If ($null -eq $NugetPackage) {
-    throw "You must have nuget.commandline installed in order to automatically update Chocolatey.lib. Alternatively you can update Chocolatey.lib in Visual Studio to Version '$ChocolateyVersion'"
+    throw "You must have nuget.commandline installed in order to automatically update Chocolatey.lib. Alternatively you can update Chocolatey.lib in Visual Studio to Version '$($ChocolateyVersion.Version)'"
 }
 
 nuget restore $PSScriptRoot/Source/ChocolateyGui.sln
